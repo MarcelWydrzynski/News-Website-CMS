84e72cd6d12698a8418dd219bc6c4a3fd2c234e4
MarcelWydrzynski <wydrzynski1999@gmail.com>
2025-03-20 19:50:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# News-Website-CMS
+ A news website with the ability of adding new articles & ads via a CMS system
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Article from "./pages/Article";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/article" element={<Article />} />{" "}
+    </Routes>
+  );
+}
+
+export default App;
